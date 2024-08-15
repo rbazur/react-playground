4d9b12a32c460529ca944167f014604b6ff4fa4c
rbazur <razur@udm.edu.ph>
2024-08-15 05:19:16 -0700

    React Push Playground

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 5271, name: "Record React Lectures", completed: true },
+    { id: 7825, name: "Edit React Lectures", completed: false },
+    { id: 8391, name: "Watch Lectures", completed: false },
+  ]);
+
   return (
     <div className="App">
-      <h1>Tropang Potchi</h1>
+      <p></p>
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li>{task.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
